d4bfaa8c26af9f67d0d7ba03b2ab8ba16cee5293
Ouadie Lahdioui <ouadiesoft@gmail.com>
2016-09-21 22:39:31 +0100

    Add Pokemon Component

---
+++ src/app/pokemon/pokemon.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PokemonComponent } from './pokemon.component';
+
+describe('Component: Pokemon', () => {
+  it('should create an instance', () => {
+    let component = new PokemonComponent();
+    expect(component).toBeTruthy();
+  });
+});
